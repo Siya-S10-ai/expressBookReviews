3bdbe7cc5f2d5c06330c4e135e24ad4dc8b0b0b4
Siya <107757390+Siya-S10-ai@users.noreply.github.com>
2026-04-20 15:41:22 +0200

    Update port and implement book lookup endpoints
    Change server PORT from 5000 to 5001. Populate book #1 reviews field and implement public book routes: /isbn/:isbn returns the book object for the given ISBN, /author/:author and /title/:title return arrays of matching books (each including an isbn field), and /review/:isbn returns the reviews for the given ISBN. These changes replace placeholder responses with functional handlers.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,26 +17,32 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const keys = Object.keys(books);
+  const author = req.params.author;
+  const result = keys.filter((key) => books[key]["author"] === author)
+  .map((key) => ({...books[key], isbn: key}));
+  res.send(result);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const keys = Object.keys(books);
+  const title = req.params.title;
+  const result = keys.filter((key) => books[key]["title"] === title)
+  .map((key) => ({...books[key], isbn: key}));
+  res.send(result);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const review = req.params.isbn;
+  res.send(books[review]["reviews"]);
 });
 
 module.exports.general = public_users;
